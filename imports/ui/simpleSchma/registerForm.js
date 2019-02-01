1c2fd8c557a88e9e6cd2a522b38ae8c4b408b3b2
VarunaLex <lex.varuna@gmail.com>
2019-02-01 19:39:09 +0530

    Validation config

--- imports/ui/simpleSchma/registerForm.js
+++ imports/ui/simpleSchma/registerForm.js
@@ -1,6 +1,6 @@
 import SimpleSchema from 'simpl-schema';
 
-const loginForm = new SimpleSchema({
+const registerForm = new SimpleSchema({
   username: {
     type: String,
     min: 3,
@@ -10,14 +10,22 @@ const loginForm = new SimpleSchema({
       variant: "outlined"
     }
   },
-  password: {
+  name: {
+    type: String,
+    min: 5,
+    uniforms: {
+      showInlineError: true,
+      variant: "outlined"
+    }
+  },
+  organization: {
     type: String, 
-    min: 3,
     uniforms: {
       showInlineError: true,
       variant: "outlined",
+      helperText: "School / institute"
     }
   }
 });
 
-export default loginForm;
+export default registerForm;
